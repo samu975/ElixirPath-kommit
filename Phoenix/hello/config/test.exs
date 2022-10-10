ec7fb2db38eb84f1de337f5ac15f38b3cd1558a9
Samuel Rosero <safero99@gotmail.com>
2022-10-10 15:22:17 -0500

    Finish the fourth module

--- Phoenix/hello/config/test.exs
+++ Phoenix/hello/config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :hello, Hello.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "5550142SaMy",
   hostname: "localhost",
   database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
--- Phoenix/hello/test/hello_web/views/error_view_test.exs
+++ Phoenix/hello/test/hello_web/views/error_view_test.exs
@@ -1,6 +1,8 @@
 defmodule HelloWeb.ErrorViewTest do
   use HelloWeb.ConnCase, async: true
 
+  @moduletag error_view_case: "some_interesting_value"
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
@@ -11,4 +13,5 @@ defmodule HelloWeb.ErrorViewTest do
   test "renders 500.html" do
     assert render_to_string(HelloWeb.ErrorView, "500.html", []) == "Internal Server Error"
   end
+
 end
